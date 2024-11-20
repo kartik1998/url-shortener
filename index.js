19e27ef74185fd6392dc47a84b3cc6befef8ffe1
kartikr <kartikr@adobe.com>
2024-11-20 14:55:26 +0530

    setup basic server

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require("express");
+const config = require("./utils/config");
 
+const app = express();
 
+app.get("/ping", (req, res) => {
+  return res.send("pong");
+});
+
+app.listen(config.PORT, () => {
+  console.log(`listening on port ${config.PORT}`);
+});
